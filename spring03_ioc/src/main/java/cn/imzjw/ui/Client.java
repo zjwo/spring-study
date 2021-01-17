bea62afe648567469586e1c8378cf05204914f3a
zjwo <garvey959@gmail.com>
2021-01-17 15:11:44 +0800

    FileSystemXmlApplicationContext 的使用

--- spring03_ioc/src/main/java/cn/imzjw/ui/Client.java
+++ spring03_ioc/src/main/java/cn/imzjw/ui/Client.java
@@ -3,7 +3,7 @@ package cn.imzjw.ui;
 import cn.imzjw.dao.IAccountDao;
 import cn.imzjw.service.IAccountService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * 模拟表现层，用于调用业务层
@@ -15,13 +15,21 @@ public class Client {
 
 	/**
 	 * 获取 spring 的 ioc 核心容器，并根据 id 获取对象
+	 * ApplicationContext 三个常用实现类:
+	 * ClassPathXmlApplicationContext: 可以加载类路径下的配置文件，要求配置文件必须在类路径下，不在的话就加载不了 ( 更常用 )
+	 * FileSystemXmlApplicationContext: 可以加载磁盘任意路径下的文件, 但是必须要有访问权限
+	 * AnnotationConfigApplicationContext: 用于读取注解创建容器的
 	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		// ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		ApplicationContext ac = new FileSystemXmlApplicationContext("C:\\Users\\garvey\\Desktop\\bean.xml");
+
 		IAccountService service = ac.getBean("accountService", IAccountService.class);
+
 		IAccountDao dao = (IAccountDao) ac.getBean("accountDao");
+
 		System.out.println(service);
 		System.out.println(dao);
 	}
