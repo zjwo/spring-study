d8e6d639fedbc1bb4cdf85ebc777b945e7fccc7c
zjwo <garvey959@gmail.com>
2021-01-18 12:52:08 +0800

    spring 的依赖注入

---
+++ spring05_di/src/main/java/cn/imzjw/service/impl/AccountServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.imzjw.service.impl;
+
+import cn.imzjw.service.IAccountService;
+
+/**
+ * @author https://blog.imzjw.cn
+ * @date 2021/1/17 11:35
+ */
+public class AccountServiceImpl implements IAccountService {
+
+	@Override
+	public void saveAccount() {
+		System.out.println("service");
+	}
+}
---
+++ spring05_di/src/main/java/cn/imzjw/ui/Client.java
@@ -0,0 +1,22 @@
+package cn.imzjw.ui;
+
+import cn.imzjw.service.IAccountService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 模拟表现层，用于调用业务层
+ *
+ * @author https://blog.imzjw.cn
+ * @date 2021/1/17 11:41
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		// ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+
+		IAccountService service = ac.getBean("accountService", IAccountService.class);
+
+		service.saveAccount();
+	}
+}
